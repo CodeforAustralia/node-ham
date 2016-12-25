6f93a862fb36a140b7422832471e93d57486fb2a
PuZZleDucK <puzzleduck@gmail.com>
2016-12-25 20:44:20 +1100

    add server for incrementer

---
+++ increment_server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+var inc = require('./increment.js');
+app.use('/', inc);
+
+
+start = function(){
+  console.log("server up at 127.0.0.1:3000")
+};
+
+app.listen(3001, "127.0.0.1", 10, start); // port, host, limit, callback
