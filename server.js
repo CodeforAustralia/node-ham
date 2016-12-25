49ae223bbe843cb4273e3aab355134275ae40180
PuZZleDucK <puzzleduck@gmail.com>
2016-12-25 20:38:40 +1100

    multi serve from single instance - fail

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var app = express();
 
-var pages = require('./response.js');
-app.use('/', pages);
+var inc = require('./increment.js');
+app.use('/increment', inc);
+
+var div2 = require('./increment.js');
+app.use('/divide', div2);
 
 start = function(){
   console.log("server up at 127.0.0.1:3000")
